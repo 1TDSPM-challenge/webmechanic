026ff512c92b266e8476910e0216f11dda81a176
Murillo77 <murillofsantanna@gmail.com>
2024-09-12 22:34:04 -0300

    Conteudo adicionado na página de equipe.

--- src/pages/Equipe.tsx
+++ src/pages/Equipe.tsx
@@ -1,10 +1,33 @@
-const Equipe = () => {
-    return (
-        <>
-            <h1>Equipe</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut purus et ipsum tristique consectetur. Nullam consequat, purus et finibus tristique, est arcu lobortis velit, vel commodo velit eros vel velit.</p>
-        </>
+import { useState } from 'react';
+import '../styles/Equipe.modules.css';
+import Murillo from '../img/banner-murillo.png';
+import Vitor from '../img/banner-vitor.png';
+import Samuel from '../img/banner-samuel.png';
+
+const images = [Murillo, Vitor, Samuel];
+
+const Equipe: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
     );
-}
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <div className="carousel">
+      <button onClick={handlePrev}>Anterior</button>
+      <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      <button onClick={handleNext}>Próximo</button>
+    </div>
+  );
+};
 
 export default Equipe;
