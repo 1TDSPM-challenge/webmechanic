0f65ced49f2c41b6322fff82376c0e60694077f8
samueldamasceno <samueldamasceno.vip@gmail.com>
2024-09-13 21:02:11 -0300

    responsividade do cartão adicionada

--- src/components/Cartao.tsx
+++ src/components/Cartao.tsx
@@ -13,6 +13,17 @@ const CardContainer = styled.div`
     position: relative;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     overflow: hidden;
+
+    @media (max-width: 768px) {
+        border-radius: 30px;
+        max-width: 100%;
+        aspect-ratio: 16 / 9;
+    }
+
+    @media (max-width: 480px) {
+        border-radius: 20px;
+        aspect-ratio: 16 / 10;
+    }
 `;
 
 const CardConteudo = styled.div`
@@ -21,16 +32,47 @@ const CardConteudo = styled.div`
     left: 40px;
     transform: translateY(-50%);
     text-align: left;
+    color: white;
+
+    @media (max-width: 768px) {
+        left: 20px;
+    }
+
+    @media (max-width: 480px) {
+        left: 15px;
+    }
 `;
 
 const CardTitulo = styled.h3`
     font-size: 1.8rem;
     margin-bottom: 30px;
+    color: white;
+
+    @media (max-width: 768px) {
+        font-size: 1.5rem;
+        margin-bottom: 20px;
+    }
+
+    @media (max-width: 480px) {
+        font-size: 1.2rem;
+        margin-bottom: 15px;
+    }
 `;
 
 const CardTexto = styled.p`
     font-size: 0.8rem;
     margin-bottom: 20px;
+    color: white;
+
+    @media (max-width: 768px) {
+        font-size: 0.7rem;
+        margin-bottom: 15px;
+    }
+
+    @media (max-width: 480px) {
+        font-size: 0.6rem;
+        margin-bottom: 10px;
+    }
 `;
 
 const Cartao = () => {
