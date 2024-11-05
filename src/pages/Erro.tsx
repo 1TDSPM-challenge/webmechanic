5355d1a98707c96b8ce5b040446b4c64a275177f
vitorisacb <vitorisac14@gmail.com>
2024-11-04 22:28:42 -0300

    imagem de erro aplicada

--- src/pages/Erro.tsx
+++ src/pages/Erro.tsx
@@ -4,9 +4,13 @@ const Erro = () => {
     return (
         <>
             <h1>Erro</h1>
+            
             <p>Página não encontrada!</p>
+            
+            <img src="/cancel_1721955.png" alt="Imagem de erro" style={{ width: '100px', height: 'auto' }} />
         </>
     );
 }
 
 export default Erro;
+
